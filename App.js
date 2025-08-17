c50e3d584705483720aed79f858a086d0cb060e2
Olaadnan-pixel <olaadnan270@gmail.com>
2025-08-17 23:41:44 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={{flex:1, alignItems:'center', justifyContent:'center'}}>
+      <Text>Magnetic Dental Lab - LIMS App</Text>
+    </View>
+  );
+}
